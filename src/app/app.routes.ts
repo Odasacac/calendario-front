161e1098e8179c04cc70da0b04fce82578ab20e0
CarlosCA <carloscasadoaguilera@gmail.com>
2026-03-25 11:44:41 +0100

    Creando la estructura login home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: "login", component: LoginComponent},
+    {path: "home", component: HomeComponent},
+    {path: "", redirectTo: "/login", pathMatch: "full"}
+];
